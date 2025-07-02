11ecbefc091e5ee658189df0c3c8fb619394f1c5
ezemas10 <ezeguevaraf@hotmail.com>
2025-07-02 02:03:47 -0300

    CreaTuLanding1+Guevara sin logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,20 @@
 import { CartWidget } from "../cartWidget/CartWidget";
 import "./Navbar.css";
-import logoCeleste from '../../assets/icons/logoCelesteTienda.png';
+//import logoCeleste from '../../assets/icons/logoCelesteTienda.png';
 
 export const Navbar = ({ contador }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-estilopropio">
       <div className="container-fluid">
 
-        <a href="#"><img className="logonav" src={logoCeleste} alt="Logo Consultorios Podológicos"/></a>
+        {/* <a href="#"><img className="logonav" src={logoCeleste} alt="Logo Consultorios Podológicos"/></a> */}
 
-          <div className="d-flex align-items-center gap-3">
-            <CartWidget />
-            <span className="counterClass"> {contador} </span> 
-          </div>
+        <h2 className="logonav">Consultorios Podológicos Tienda</h2>
+
+        <div className="d-flex align-items-center gap-3">
+          <CartWidget />
+          <span className="counterClass"> {contador} </span> 
+        </div>
 
       </div>
     </nav>
